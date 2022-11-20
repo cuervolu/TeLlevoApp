0a374e0109722c4a4fb600654257112c7b1ae50e
cuervolu <cuervolu@protonmail.com>
2022-11-19 23:45:53 -0300

    fix: cambio de API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    projectId: 'tellevoapp-003d',
-    appId: '1:644568799794:web:73d37a5f4378387f647a2b',
-    storageBucket: 'tellevoapp-003d.appspot.com',
-    apiKey: 'AIzaSyC56I8njOfkRHVkylxjHAKTvAiNTXZFUVs',
+    apiKey: 'AIzaSyCMdPb9l09zMK_3QmNv9zu08N_cT-AghEQ',
     authDomain: 'tellevoapp-003d.firebaseapp.com',
+    projectId: 'tellevoapp-003d',
+    storageBucket: 'tellevoapp-003d.appspot.com',
     messagingSenderId: '644568799794',
+    appId: '1:644568799794:web:73d37a5f4378387f647a2b'
   },
   mapsKey: 'AIzaSyAQbm3-836j1pEBTPlmu9oeOK2BWFNIEBM',
   production: false,
