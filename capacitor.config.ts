6f8148f5684a360d1b9db1d435861ebe10fc01a1
cuervolu <cuervolu@protonmail.com>
2022-11-29 13:49:11 -0300

    feat: se añade splash screen e icono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cuervolu.tellevoapp',
   appName: 'TeLlevoApp',
   webDir: 'www',
   bundledWebRuntime: false
